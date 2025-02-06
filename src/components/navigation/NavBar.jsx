6b84199d8f24785cff5b1d4d6a37c47e29dd07c5
Chrismar <chrismar303@gmail.com>
2025-02-06 01:44:19 -0800

    Custom Colors and Navbar update
    index.css
    * Created custom primary color variable
    * updated background to match figma design
    
    Navbar
    * Updated to match closer to figma design
    * Added Contact button

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -5,14 +5,17 @@ export default function NavBar() {
         Search: '/search' 
     }
     return (
-        <nav className="nav-main h-14 flex justify-between items-center bg-red-600 px-8 border-b border-gray-500 drop-shadow-lg">
+        <nav className="nav-main h-14 flex justify-between items-center px-8 border-b border-gray-100 drop-shadow-lg text-black bg-white">
             <NavLogo />
-            <ul className="flex list-none gap-x-8">
+            <ul className="flex place-items-center list-none gap-x-8 text-(--primary-color)">
                 {Object.entries(navOptions).map(([option, url]) => (
                     <li key={url} className="cursor-pointer">
                         <a href={ url }>{ option }</a>
                     </li>
                 ))}
+                <button className="px-3 py-2.5 rounded-3xl drop-shadow-md bg-(--primary-color) text-white cursor-pointer">
+                    Contact
+                </button>
             </ul>
         </nav>
     )
