2e09c60f47d8a4972887072cf18648f524557cf1
chrismar303 <chrismar303@gmail.com>
2025-02-06 23:36:39 -0800

    Update Banner To match ui | Removed h1 styles (#1)
    App.jsx
    * Updated "Wiki & Go+" to "Wiki Go +"
    * Added the blue line
    * Added the subheader text
    * Added styles to match ui design
    
    index.css
    * Removed h1 styles

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,12 @@ function App() {
   return (
     <>
       <NavBar />
-      <div>
-        <h1>Wiki & GO+</h1>
+      <div className="text-(--primary-color) drop-shadow">
+        <div className="flex flex-col place-items-center py-8">
+          <h1 className="text-6xl font-bold">Wiki GO +</h1>
+          <hr className="w-[80%] my-4" />
+          <h3 className="text-4xl text-black font-thin">Your Wiki Bud On The Go</h3>
+        </div>
         <SearchForm className="min-h-100 flex place-items-center place-content-center flex-col bg-red-100" />
       </div>
     </>
