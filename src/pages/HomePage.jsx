26d28b7a1c768e82410e21df461b4f66e33929bb
chrismar303 <chrismar303@gmail.com>
2025-02-08 00:09:24 -0800

    Added React Router To Project (#9)
    App.jsx
    * Refactored to use Router components
    
    HomePage.jsx
    *  Refactored Homepage html from App.jsx so it could be use as a `page`
       for the `Route` component
    
    NavBar
    * Used `NavLink` instead of `a` for Router

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import SearchForm from '../components/search/SearchForm'
+import wikiImage from '../assets/images/wikipedia-logo.png'
+
+export default function HomePage() {
+    return (
+        <div className="m-8 flex flex-col place-items-center text-(--primary-color) drop-shadow">
+            <div className="w-full flex flex-col place-items-center py-8">
+              <h1 className="text-6xl font-bold">Wiki GO +</h1>
+              <hr className="w-[80%] my-4" />
+              <h3 className="text-4xl text-black font-thin">Your Wiki Bud On The Go</h3>
+            </div>
+            <SearchForm className="min-h-100 flex place-items-center place-content-center flex-col bg-red-100" />
+            <img src={wikiImage} alt="Wikipedia Globe Logo" className="w-[300px] h-[270px] mt-16" />
+        </div>
+    )
+}
