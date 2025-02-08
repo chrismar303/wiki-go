07a9da506e62170d72d14f16d79fcf063dee2c70
Chrismar <chrismar303@gmail.com>
2025-02-08 00:20:18 -0800

    [Feat] Added SectionDivider Component
    SectionDivider.jsx
    * This will serve as a reusable componennt for divided headers and the
      page's content
    
    HomePage.jsx
    * Replace the `hr` tag with the `SectionDivider` component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import SearchForm from '../components/search/SearchForm'
 import wikiImage from '../assets/images/wikipedia-logo.png'
+import SectionDivider from '../components/divider/SectionDivider'
 
 export default function HomePage() {
     return (
         <div className="m-8 flex flex-col place-items-center text-(--primary-color) drop-shadow">
             <div className="w-full flex flex-col place-items-center py-8">
               <h1 className="text-6xl font-bold">Wiki GO +</h1>
-              <hr className="w-[80%] my-4" />
+              <SectionDivider className="w-[80%] my-4" />
               <h3 className="text-4xl text-black font-thin">Your Wiki Bud On The Go</h3>
             </div>
             <SearchForm className="min-h-100 flex place-items-center place-content-center flex-col bg-red-100" />
