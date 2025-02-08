c60a20097f92a5243ee6dbe0c01c345dba3dd7a8
chrismar303 <chrismar303@gmail.com>
2025-02-07 23:32:19 -0800

    [Fixed] ActionButton color is now visible (#7)
    * Button was appearing white because color property wasn't applied. This occured because during testing, the parent component was applying the color

--- src/components/buttons/ActionButton.jsx
+++ src/components/buttons/ActionButton.jsx
@@ -1,6 +1,6 @@
 export default function ActionButton({ children }) {
     return (
-        <button className="px-5 py-2 border-3 rounded-lg font-light bg-white drop-shadow">
+        <button className="px-5 py-2 border-3 rounded-lg font-light bg-white drop-shadow text-(--primary-color)">
             {children}
         </button>
     )
