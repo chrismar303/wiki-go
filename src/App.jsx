eb49e9a03e545b4ce6dc95a2e96dca023332c62d
chrismar303 <chrismar303@gmail.com>
2025-02-07 20:06:49 -0800

    Added Wiki Logo to Home page (#6)
    App.css
    * Modified so that the image and all content will be centered
    
    App.jsx
    * Added image to home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import './App.css'
 import NavBar from './components/navigation/NavBar'
 import SearchForm from './components/search/SearchForm'
+import wikiImage from './assets/images/wikipedia-logo.png'
 
 function App() {
   return (
     <>
       <NavBar />
-      <div className="text-(--primary-color) drop-shadow">
-        <div className="flex flex-col place-items-center py-8">
+      <div className="m-8 flex flex-col place-items-center text-(--primary-color) drop-shadow">
+        <div className="w-full flex flex-col place-items-center py-8">
           <h1 className="text-6xl font-bold">Wiki GO +</h1>
           <hr className="w-[80%] my-4" />
           <h3 className="text-4xl text-black font-thin">Your Wiki Bud On The Go</h3>
         </div>
         <SearchForm className="min-h-100 flex place-items-center place-content-center flex-col bg-red-100" />
+        <img src={wikiImage} alt="Wikipedia Globe Logo" className="w-[300px] h-[270px] mt-16" />
       </div>
     </>
   )
