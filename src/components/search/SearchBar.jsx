4d3f799f3c5fe489068a7294af68c11ac3c3cb0f
Chrismar <chrismar303@gmail.com>
2025-02-07 23:43:23 -0800

    [Fixed] Added missing color to SearchBar Icon

--- src/components/search/SearchBar.jsx
+++ src/components/search/SearchBar.jsx
@@ -8,7 +8,7 @@ export default function SearchBar() {
                 placeholder="My Search Text"
                 className="px-6 py-2 min-w-100 w-40% max-w-624 border-3 border-(--primary-color) rounded-3xl drop-shadow-xl bg-white text-black font-light"
             />
-            <IoSearch className="absolute right-4 top-[15%] text-3xl" />
+            <IoSearch className="absolute right-4 top-[15%] text-3xl text-(--primary-color)" />
         </div>
     )
 }
