ecbafb16c104325e24a7aa07622b39b93b1f7d6e
chrismar303 <chrismar303@gmail.com>
2025-02-11 16:18:39 -0800

    [Epic] Search Page (#16)
    * Added React Router To Project (#9)
    
    App.jsx
    * Refactored to use Router components
    
    HomePage.jsx
    *  Refactored Homepage html from App.jsx so it could be use as a `page`
       for the `Route` component
    
    NavBar
    * Used `NavLink` instead of `a` for Router
    
    * [Feat] Added SectionDivider Component (#10)
    
    SectionDivider.jsx
    * This will serve as a reusable componennt for divided headers and the
      page's content
    
    HomePage.jsx
    * Replace the `hr` tag with the `SectionDivider` component
    
    * [Feat] Section Page - 1: Seach Header (#11)
    
    SearchPage.jsx
    * Added the header section for the search page
    
    App.jsx
    * Added a `Route` for the `SearchPage` component
    
    App.css
    * Removed `text-align`
    - This was shifting all text to the center. This caused our header text
      to be centered
    
    SearchForm
    * Fixed incomplete name
    * Removed `place-content-center` class. This was always centering our
      search component. This should be done by its parent instead
    
    * [Feat] Search Result Card (#12)
    
    SearchResultCard.jsx
    * Component that list the details of a search result on the `SearchPage`
    * It list the title, introduction, and image of the article
    
    * [Feat] Added Search Result list section (#14)
    
    SearchPage.jsx
    * Added a section that displays the Search results using the
      `SearchResultCard` component
    * Currently is using test data
    
    * [Fix] SectionDivider undefined in className (#15)
    
    SectionDivider
    * If no className is provided, then an empty string is given instead.
    * This change stops undefined from appearing in `class` attribute

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -1,3 +1,5 @@
+import { NavLink } from 'react-router'
+
 export default function NavBar() {
     const navOptions = {
         Home: '/',
@@ -10,7 +12,7 @@ export default function NavBar() {
             <ul className="flex place-items-center list-none gap-x-8 text-(--primary-color)">
                 {Object.entries(navOptions).map(([option, url]) => (
                     <li key={url} className="cursor-pointer">
-                        <a href={ url }>{ option }</a>
+                        <NavLink to={ url }>{ option }</NavLink>
                     </li>
                 ))}
                 <button className="px-3 py-2.5 rounded-3xl drop-shadow-md bg-(--primary-color) text-white cursor-pointer">
@@ -23,6 +25,6 @@ export default function NavBar() {
 
 function NavLogo() {
     return (
-        <span>Chrismar</span>
+        <NavLink to="/">Chrismar</NavLink>
     )
 }
