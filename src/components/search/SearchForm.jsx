aabdc71e5298ce007b1476a6d263010e2b529364
Chrismar <chrismar303@gmail.com>
2025-03-02 23:04:09 -0800

    [FEAT] Search API Connect 1: Form Refactor
    SearchForm.jsx
    * Added state for tracking the searchbar text
    * Each time searchbar text changes, the text it printed to the console
    
    SearchBar.jsx
    * Defined props to accept the `state` variable from its parent

--- src/components/search/SearchForm.jsx
+++ src/components/search/SearchForm.jsx
@@ -1,10 +1,20 @@
 import SearchBar from './SearchBar'
 import ActionButton from '../buttons/ActionButton'
+import {useState, useEffect} from 'react'
 
 export default function SearchForm() {
+  const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    console.log('Search: ', searchTerm)
+  }, [searchTerm])
+
   return (
     <div className="flex gap-8">
-      <SearchBar />
+      <SearchBar
+        searchTerm={searchTerm}
+        onChange={event => setSearchTerm(event.target.value)}
+      />
       <ActionButton>Search</ActionButton>
     </div>
   )
