4ce8ba7fd58c1b2c2d9c6bb60f97bdce9d246a3a
chrismar303 <chrismar303@gmail.com>
2025-03-02 18:17:27 -0800

    Epic article display page (#21)
    * [Feat] ArticleNav: Chapter Navbar outline (#17)
    
    ArticleNav.jsx
    * Links to sections based on the articles chapters
    
    * [Feat] Created ArticlePage & Header section (#18)
    
    ArticlePage.jsx
    * This page will display the contents of the chosen article from the
      `SearchPage`
    * Implemented the header section for the Article Page
    
    * Prettier formatting
    
    * [Feat] ArticlePage: Article Section Segements (#19)
    
    ArticlePage.jsx
    * Added the section where the contents of the article will be display
    * Added more spacing around page header
    * moved mockdata into a `article` variable
    
    * [FEAT] ArticlePage: Improved Section layouts (#20)
    
    ArticlePage.jsx
    * Added new dividers on the left and circular markers to signify
      different segments
    * Some prettier formatting

---
+++ src/pages/ArticlePage.jsx
@@ -0,0 +1,67 @@
+import ArticleNav from '../components/navigation/ArticleNav'
+import SectionDivider from '../components/divider/SectionDivider'
+export default function ArticlePage() {
+  const article = {
+    title: 'Title of Article',
+    date: '07/77/7777',
+    text: 'Velit anim labore ullamco officia deserunt dolor aliqua. Aliquip fugiat duis id Lorem. Occaecat sunt id consequat in in laborum in consequat id aliquip. Lorem ea est veniam est anim sint pariatur.',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgUO36r9PwjwIlVO9CLAC4__aHRcwZJNpq8Q&s',
+    sections: ['Intro', 'History', 'Production', 'Adaptation', 'External'], // TODO: this passed to ArticleNav
+  }
+  return (
+    <>
+      <ArticleNav />
+      <section className="my-8 h-full w-[80%] mx-auto">
+        <div className="flex mb-8 place-content-between">
+          <h1 className="text-4xl text-(--primary-color) font-bold drop-shadow">
+            {article.title}
+          </h1>
+          <span className="text-sm text-gray-500 self-end font-semibold">
+            {article.date}
+          </span>
+        </div>
+        <div className="w-full h-128 relative rounded-2xl overflow-hidden">
+          <img
+            className="w-full object-cover max-w-full h-full"
+            src={article.img}
+          />
+        </div>
+      </section>
+      {/* Article Sections */}
+      <div className="bg-white drop-shadow-2xl">
+        <div className="flex flex-col w-[80%] mx-auto">
+          {article.sections.map((section, index) => (
+            <div key={index} className="w-[90%] mx-auto">
+              <div className="flex my-4 gap-8 items-center">
+                <div className="bg-white drop-shadow-2xl border-2 border-(--primary-color) rounded-full h-5 w-5 -mx-2"></div>
+                <div className="bg-gray-100 flex-1 rounded h-1 shadow-md"></div>
+              </div>
+              <section className="pl-8 border-l-2 border-(--primary-color)">
+                <div className="flex gap-8 text-2xl font-medium mb-6">
+                  <span className="rounded w-8 text-center text-white bg-(--primary-color)">
+                    {index}
+                  </span>
+                  <h3 className="text-gray-600">{section.toUpperCase()}</h3>
+                </div>
+
+                <SectionDivider />
+                <p className="text-black font-light mt-6">
+                  Ipsum in voluptate commodo sit sunt velit dolore ex nisi
+                  ullamco cupidatat esse. Reprehenderit exercitation labore esse
+                  eu deserunt consectetur excepteur mollit consequat ipsum
+                  proident ipsum excepteur. Do mollit proident fugiat dolore
+                  minim. Quis dolor officia aliqua Lorem ipsum excepteur anim
+                  quis elit adipisicing fugiat ullamco esse excepteur. Laborum
+                  occaecat dolore occaecat in culpa incididunt veniam. Sit
+                  pariatur aliquip duis proident minim aute veniam. Culpa velit
+                  duis pariatur mollit dolore excepteur adipisicing sunt
+                  deserunt.
+                </p>
+              </section>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
