f0de9795678cbb7286076e13542aa3891bfd2343
chrismar303 <chrismar303@gmail.com>
2025-03-14 01:39:54 -0700

    [Feat] Divide Article Text Into 4 Sections (#31)
    * [FEAT] Article Text Split Into 4 Sections
    
    ArticlePage.jsx
    * articleText is divided into 4 different parts
    * Removed unneeded code
    
    * [FEAT] Refactor ArticleNav (#30)
    
    ArticleNav.jsx
    * Changed to list out `Part 1` to `Part N`
    * Added Prop Validation

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -8,48 +8,53 @@ export default function ArticlePage() {
   const {title} = useParams()
   const [articleTitle, setArticleTitle] = useState('')
   const [articleText, setArticleText] = useState('')
+  const [articleSections, setArticleSections] = useState([])
+  const NUM_PARTS = 4
 
   useEffect(() => {
+    function divideIntoSections(text) {
+      // divide into NUM_PARTS (4) segments
+      const articleSegments = []
+      const stepSize = Number(text.split('.').length / NUM_PARTS)
+      for (let i = 0; i < text.split('.').length; i += stepSize)
+        articleSegments.push(text.split('.').slice(i, i + stepSize))
+      return articleSegments
+    }
+
     async function fetchArticle() {
       const response = await axios.get(
         `${import.meta.env.VITE_API_URL}/article/${title}`
       )
       setArticleTitle(response.data.title)
       setArticleText(response.data.text)
+      setArticleSections(divideIntoSections(response.data.text))
     }
     fetchArticle()
   }, [])
 
-  const article = {
-    title: 'Title of Article',
-    date: '07/77/7777',
-    text: 'Velit anim labore ullamco officia deserunt dolor aliqua. Aliquip fugiat duis id Lorem. Occaecat sunt id consequat in in laborum in consequat id aliquip. Lorem ea est veniam est anim sint pariatur.',
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgUO36r9PwjwIlVO9CLAC4__aHRcwZJNpq8Q&s',
-    sections: ['Intro', 'History', 'Production', 'Adaptation', 'External'] // TODO: this passed to ArticleNav
-  }
   return (
     <>
-      <ArticleNav />
+      <ArticleNav numParts={NUM_PARTS} />
       <section className="my-8 h-full w-[80%] mx-auto">
         <div className="flex mb-8 place-content-between">
           <h1 className="text-4xl text-(--primary-color) font-bold drop-shadow">
             {articleTitle}
           </h1>
-          <span className="text-sm text-gray-500 self-end font-semibold">
-            {article.date}
+          <span className="text-sm text-gray-700 self-end font-semibold">
+            07/77/7777
           </span>
         </div>
         <div className="w-full h-128 relative rounded-2xl overflow-hidden">
           <img
             className="w-full object-cover max-w-full h-full"
-            src={article.img}
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgUO36r9PwjwIlVO9CLAC4__aHRcwZJNpq8Q&s"
           />
         </div>
       </section>
       {/* Article Sections */}
       <div className="bg-white drop-shadow-2xl">
         <div className="flex flex-col w-[80%] mx-auto">
-          {article.sections.map((section, index) => (
+          {articleSections.map((section, index) => (
             <div key={index} className="w-[90%] mx-auto">
               <div className="flex my-4 gap-8 items-center">
                 <div className="bg-white drop-shadow-2xl border-2 border-(--primary-color) rounded-full h-5 w-5 -mx-2"></div>
@@ -58,13 +63,13 @@ export default function ArticlePage() {
               <section className="pl-8 border-l-2 border-(--primary-color)">
                 <div className="flex gap-8 text-2xl font-medium mb-6">
                   <span className="rounded w-8 text-center text-white bg-(--primary-color)">
-                    {index}
+                    {index + 1}
                   </span>
-                  <h3 className="text-gray-600">{section.toUpperCase()}</h3>
+                  <h3 className="text-gray-600">Part {index + 1}</h3>
                 </div>
 
                 <SectionDivider />
-                <p className="text-black font-light mt-6">{articleText}</p>
+                <p className="text-black font-light mt-6">{section}</p>
               </section>
             </div>
           ))}
