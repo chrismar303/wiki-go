6535918f83ab40d53d3a639f2d0dbd437b462113
Chrismar <chrismar303@gmail.com>
2025-03-13 23:58:57 -0700

    Removed title from dependecy array

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -18,7 +18,7 @@ export default function ArticlePage() {
       setArticleText(response.data.text)
     }
     fetchArticle()
-  }, [title])
+  }, [])
 
   const article = {
     title: 'Title of Article',
