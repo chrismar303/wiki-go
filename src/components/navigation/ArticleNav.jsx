dbedcb0525c5acd53d3b79fa983e0b21539504c6
Chrismar <chrismar303@gmail.com>
2025-03-14 01:31:21 -0700

    [FEAT] Refactor ArticleNav
    ArticleNav.jsx
    * Changed to list out `Part 1` to `Part N`
    * Added Prop Validation

--- src/components/navigation/ArticleNav.jsx
+++ src/components/navigation/ArticleNav.jsx
@@ -1,13 +1,16 @@
-export default function ArticleNav() {
-    return (
-        <nav className="bg-white py-6 z-10">
-            <ol className="flex flex-row place-content-center gap-16 text-gray-400 font-medium text-md cursor-pointer">
-                <li>Intro</li>
-                <li>History</li>
-                <li>Production</li>
-                <li>Adaptation</li>
-                <li>External</li>
-            </ol>
-        </nav>
-    )
+import PropTypes from 'prop-types'
+export default function ArticleNav({numParts}) {
+  return (
+    <nav className="bg-white py-6 z-10">
+      <ol className="flex flex-row place-content-center gap-16 text-gray-400 font-medium text-md cursor-pointer">
+        {Array.from(Array(numParts).keys()).map(partNum => (
+          <li key={partNum}>Part {partNum + 1}</li>
+        ))}
+      </ol>
+    </nav>
+  )
+}
+
+ArticleNav.propTypes = {
+  numParts: PropTypes.number.isRequired
 }
