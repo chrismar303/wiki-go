14d524bc184515eab24de57f29a27ecc42a0b926
Chrismar <chrismar303@gmail.com>
2025-03-15 01:47:43 -0700

    [FEAT] Home UI Update to match Design
    NavBar.jsx
    * Increased height
    * Added LOGO
    * Added onHover animations
    ** NavLink on text underline
    ** LOGO scale
    ** Contact button color invertions
    
    SearchBar.jsx
    * Removed onClick weird black outlining
    * Removed unneeded code
    
    ActionButton.jsx
    * onHover color inverting

--- src/components/buttons/ActionButton.jsx
+++ src/components/buttons/ActionButton.jsx
@@ -4,7 +4,8 @@ export default function ActionButton({onClick, children}) {
   return (
     <button
       onClick={onClick}
-      className="px-5 py-2 border-3 rounded-lg font-light bg-white drop-shadow text-(--primary-color) cursor-pointer">
+      className="px-5 py-2 bg-white hover:bg-(--primary-color) text-(--primary-color) hover:text-white 
+                 border-3 hover:border-0 rounded-lg font-light drop-shadow cursor-pointer ease-in duration-300">
       {children}
     </button>
   )
